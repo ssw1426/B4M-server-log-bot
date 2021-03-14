3bf126a24037d13181d4e6fe58a289176cce8b38
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-15 02:12:18 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    game = discord.Game('서버 입퇴장 기록')
+    await client.change_presence(status=discord.Status.online, activity=game)
+    print("봇이 온라인으로 변경되었습니다")
+
+@client.event
+async def on_member_join(member):
+    embed = discord.Embed(title="[서버 입장 로그]", description=f"{member.mention} 님께서 입장하셨습니다", color=0x62c1cc)
+    await client.get_channel(820657549018660864).send(embed=embed)
+
+@client.event
+async def on_member_remove(member):
+    embed = discord.Embed(title="[서버 퇴장 로그]", description=f"**{member}** 님께서 퇴장하셨습니다", color=0x62c1cc)
+    await client.get_channel(820657549018660864).send(embed=embed)
+
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
