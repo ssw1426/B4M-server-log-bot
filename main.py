8517133efaba3412629796a7f462de685a9410c2
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-15 02:21:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     embed = discord.Embed(title="[서버 입장 로그]", description=f"{member.mention} 님께서 입장하셨습니다", color=0x62c1cc)
-    await client.get_channel(820657549018660864).send(embed=embed)
+    await client.get_channel(802236153608536114).send(embed=embed)
 
 @client.event
 async def on_member_remove(member):
     embed = discord.Embed(title="[서버 퇴장 로그]", description=f"**{member}** 님께서 퇴장하셨습니다", color=0x62c1cc)
-    await client.get_channel(820657549018660864).send(embed=embed)
+    await client.get_channel(802236204649152623).send(embed=embed)
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
